ffec04be92be5f02742d75fa15ac5a7bcc21f7c3
Aya <shansharova_ayazhan@mail.ru>
2021-04-15 21:52:08 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Block_content from './components/Block_content';
+import Curses_which_teach from './components/Curses_which_teach';
 
 function App() {
   return (
     <div className="App">
       <Block_content/>
+      <Curses_which_teach/>
     </div>
   );
 }
--- src/components/Curses_which_teach.js
+++ src/components/Curses_which_teach.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import "./Curses_which_teach.css"
 
 export default function Curses_which_teach() {
     return (
-        <div>
-            
+        <div className="wrapper_curses_which_teach">
+            <div className="curses_which_teach_theme">
+                <p>Курсы которые преподает</p>
+            </div>
+            <div className="marketing">
+                <div className="border_for_line">
+                    <p className="pink_color">Маркетинг и PR</p>
+                    <span>Создание и продвижение <br/>бренда работодателя</span>
+                </div>
+                <div className="marketing_author">
+                    <span>22 февраля - 22 марта</span>
+                    <p>Ведет: Татьяна Лукинюк</p>
+                </div>
+                
+
+            </div>
         </div>
     )
 }
